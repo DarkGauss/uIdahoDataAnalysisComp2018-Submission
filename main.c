5cd98ce866c18855b6f3783ff904387b2b876abc
Robert Goes <RobertRGoes@gmail.com>
2019-04-10 22:14:21 -0700

    Added a lot

---
+++ main.c
@@ -0,0 +1,61 @@
+/*
+	This file is part of CGP-Library
+	Copyright (c) Andrew James Turner 2014 (andrew.turner@york.ac.uk)
+
+    CGP-Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CGP-Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with CGP-Library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include "cgp.h"
+
+int main(void){
+
+	struct parameters *params = NULL;
+	struct dataSet *trainingData = NULL;
+	struct chromosome *chromo = NULL;
+
+	int numInputs = 26;
+	int numNodes = 30;
+	int numOutputs = 3;
+	int nodeArity = 2;
+
+	int numGens = 100000;
+	int updateFrequency = 500;
+
+	double recurrentConnectionProbability = 0.10;
+
+	params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
+
+	addNodeFunction(params, "add,sub,mul,div");
+
+	setUpdateFrequency(params, updateFrequency);
+
+	setRecurrentConnectionProbability(params, recurrentConnectionProbability);
+
+	printParameters(params);
+
+	trainingData = initialiseDataSetFromFile("training2013.data");
+
+	chromo = runCGP(params, trainingData, numGens);
+
+	printChromosome(chromo, 0);
+	saveChromosomeDot(chromo, 0, "chromo_run1.dot");
+	saveChromosomeLatex(chromo, 0, "chromo_run1.tex");
+
+	freeDataSet(trainingData);
+	freeChromosome(chromo);
+	freeParameters(params);
+
+	return 0;
+}
